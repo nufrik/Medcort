6672fcc5201294d1eace49a18d0025f355687448
nufrik <daniilonufriev30@gmail.com>
2023-04-24 14:55:43 +0300

    added Admin seed

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,21 @@
+<?php
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class AdminSeeder extends Seeder
+{
+    public function run()
+    {
+        User::insert([
+            'name' => 'admin',
+            'email'  => 'admin@domain.com',
+            'password'  => Hash::make('password'),
+        ]);
+    }
+}
+?>

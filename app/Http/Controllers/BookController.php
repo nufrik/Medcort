058e81c2a4dc8affe96bf2d2a96e9a6b7ecb6ac0
nufrik <daniilonufriev30@gmail.com>
2023-04-25 14:46:46 +0300

    add book controller

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Books;
+use App\Models\Category;
+use App\Services\BookServiceInterface;
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    private BookServiceInterface $bookService;
+
+    public function __construct(BookServiceInterface $bookService)
+    {
+        $this->bookService = $bookService;
+    }
+
+    public function showBooks($slug)
+    {
+        return $this->bookService->getBooks($slug);
+    }
+}

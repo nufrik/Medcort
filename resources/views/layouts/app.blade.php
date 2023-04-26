34057632c60d423049d917fb920d9f5fe78de39b
nufrik <daniilonufriev30@gmail.com>
2023-04-26 13:32:46 +0300

    change app blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,8 +1,33 @@
 @extends('layouts.base')
 
 @section('body')
+
     @yield('content')
-    
+
+
+    @if (Route::has('login'))
+
+        <div class="p-6 text-right sm:fixed sm:top-0 sm:right-0">
+
+            @auth
+                @if(Auth::user()->role_id == 3)
+                    <a href="{{ url('/admin') }}" class="mr-3 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-indigo-500">Админка</a>
+
+
+                @elseif(Auth::user()->role_id == 1)
+                    <a href="{{ url('/employee') }}" class="mr-3 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-indigo-500">Сотрудник</a>
+                @endif
+                    <a href="{{ url('/logout') }}" class="mr-3 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-indigo-500">Выйти</a>
+            @else
+                <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-indigo-500">{{ 'Войти' }}</a>
+
+                @if (Route::has('register'))
+                    <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-indigo-500">{{ 'Регистрация' }}</a>
+                @endif
+            @endauth
+        </div>
+    @endif
+
     @isset($slot)
         {{ $slot }}
     @endisset

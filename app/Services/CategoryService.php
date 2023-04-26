4e9ec39e268c698077faf9ef59c583a7ba950ccb
nufrik <daniilonufriev30@gmail.com>
2023-04-26 13:38:03 +0300

    добавил возвращаемы тип

--- app/Services/CategoryService.php
+++ app/Services/CategoryService.php
@@ -3,11 +3,12 @@
 namespace App\Services;
 
 use App\Models\Category;
+use Illuminate\Database\Eloquent\Collection;
 
 class CategoryService implements CategoryServiceInterface
 {
 
-    public function getByAll()
+    public function getByAll() : Collection
     {
         return Category::all();
     }

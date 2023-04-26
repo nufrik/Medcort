a5c386735cf7e4e5232f3a94a95c9e5c8c257d4b
nufrik <daniilonufriev30@gmail.com>
2023-04-26 13:42:46 +0300

    moved the header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,19 +4,7 @@
 
     <div class="relative min-h-screen bg-gray-100 bg-center sm:flex sm:justify-center sm:items-center bg-dots dark:bg-gray-900 selection:bg-indigo-500 selection:text-white">
 
-        @if (Route::has('login'))
-            <div class="p-6 text-right sm:fixed sm:top-0 sm:right-0">
-                @auth
-                    <a href="{{ url('/home') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-indigo-500">Реестр</a>
-                @else
-                    <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-indigo-500">{{ 'Войти' }}</a>
 
-                    @if (Route::has('register'))
-                        <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-indigo-500">{{ 'Регистрация' }}</a>
-                    @endif
-                @endauth
-            </div>
-        @endif
 
         <div class="p-6 mx-auto max-w-7xl lg:p-8">
             <div class="text-5xl font-extrabold ...">

bd2462b2d462d7b92ee254539d2430f6fe0c6539
nufrik <daniilonufriev30@gmail.com>
2023-04-26 13:40:55 +0300

    added relationships

--- app/Models/Roles.php
+++ app/Models/Roles.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Roles extends Model
 {
     use HasFactory;
+
+    public function users()
+    {
+        return $this->hasMany(User::class);
+    }
 }

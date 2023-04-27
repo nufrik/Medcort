bafdbdd10cf6585d553d3b61b2fdf6adfcd8a615
nufrik <daniilonufriev30@gmail.com>
2023-04-27 15:17:26 +0300

    added new actions changeUserRoleById and deleteUserById

--- app/Services/AdminServiceInterface.php
+++ app/Services/AdminServiceInterface.php
@@ -3,7 +3,6 @@
 namespace App\Services;
 
 
-
 use Illuminate\Database\Eloquent\Collection;
 
 interface AdminServiceInterface
@@ -13,4 +12,8 @@ interface AdminServiceInterface
     public function getAllCategories() : Collection;
 
     public function getAllBooks() : Collection;
+
+    public function changeUserRoleById(int $id) : bool;
+
+    public function deleteUserById(int $id) : bool;
 }

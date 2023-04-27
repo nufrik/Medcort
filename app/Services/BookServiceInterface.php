38dacc8056d304c6c7977dcbfd0296d01c9d8a59
nufrik <daniilonufriev30@gmail.com>
2023-04-27 11:06:59 +0300

    change getBookBySlug action

--- app/Services/BookServiceInterface.php
+++ app/Services/BookServiceInterface.php
@@ -9,5 +9,7 @@ interface BookServiceInterface
 {
     public function getBooksByCategorySlug(string $slug) : LengthAwarePaginator;
 
-    public function getBookByCategorySlug(string $slug, string $bookSlug) : Books;
+    /*public function getBookByCategorySlug(string $slug, string $bookSlug) : Books;*/
+
+    public function getBookBySlug(string $slug) : Books;
 }

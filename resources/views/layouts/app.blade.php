ea670c3b4fa3d8e830800df3b7a2d4249c99cde3
nufrik <daniilonufriev30@gmail.com>
2023-04-27 15:18:31 +0300

    added name user

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,16 +11,21 @@
 
             @auth
                 @if(Auth::user()->role_id == 3)
+                    <a href="" class="mr-3 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-indigo-500">Вы зашли как: {{ Auth::user()->name }}</a>
+
                     <a href="{{ url('/admin') }}" class="mr-3 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-indigo-500">Админка</a>
 
 
                 @elseif(Auth::user()->role_id == 1)
+                    <a href="" class="mr-3 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-indigo-500">Вы зашли как:  {{ Auth::user()->name }}</a>
                     <a href="{{ url('/employee') }}" class="mr-3 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-indigo-500">Сотрудник</a>
+                @elseif(Auth::user()->role_id == 2)
+                    <a href="" class="mr-3 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-indigo-500">Вы зашли как:  {{ Auth::user()->name }}</a>
                 @endif
                     <a href="{{ url('/logout') }}" class="mr-3 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-indigo-500">Выйти</a>
             @else
-                <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-indigo-500">{{ 'Войти' }}</a>
 
+                <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-indigo-500">{{ 'Войти' }}</a>
                 @if (Route::has('register'))
                     <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-indigo-500">{{ 'Регистрация' }}</a>
                 @endif

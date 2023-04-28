35b4d8b42d95cbf082f63c940678c12d516e0418
nufrik <daniilonufriev30@gmail.com>
2023-04-28 12:26:51 +0300

    added 3 actions

--- app/Services/CategoryService.php
+++ app/Services/CategoryService.php
@@ -2,6 +2,8 @@
 
 namespace App\Services;
 
+use App\Http\Requests\Category\CreateRequest;
+use App\Http\Requests\Category\EditRequest;
 use App\Models\Category;
 use Illuminate\Database\Eloquent\Collection;
 
@@ -11,5 +13,34 @@ class CategoryService implements CategoryServiceInterface
     public function getByAll() : Collection
     {
         return Category::all();
+    }
+
+    public function createCategory(CreateRequest $request) : bool
+    {
+        $data = $request->validated();
+        $translit = str_slug($data['title']);
+        $category = new Category();
+        $category->title = $request->input('title');
+        $category->slug = $translit;
+
+        return $category->save();
+    }
+
+    public function editCategoryById(int $id, EditRequest $request) : bool
+    {
+        $category = Category::findOrFail($id);
+
+        $data = $request->validated();
+        $translit = str_slug($data['title']);
+
+        $category->title = $request->input('title');
+        $category->slug = $translit;
+
+        return $category->save();
+    }
+
+    public function deleteCategoryById(int $id) : bool
+    {
+        return Category::destroy($id);
     }
 }

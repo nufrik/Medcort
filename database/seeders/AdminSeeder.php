3120b24c43e6efb2da313a03f6ecb08cae442464
nufrik <daniilonufriev30@gmail.com>
2023-04-28 12:25:38 +0300

    added two users

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -2,20 +2,33 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
+
 
 class AdminSeeder extends Seeder
 {
     public function run()
     {
         User::insert([
-            'name' => 'admin',
-            'email'  => 'admin@domain.com',
-            'role_id'  => 3,
-            'password'  => Hash::make('password'),
+            [
+                'name' => 'admin',
+                'email'  => 'admin@domain.com',
+                'role_id'  => 3,
+                'password'  => Hash::make('password'),
+            ],
+            [
+                'name' => 'employee',
+                'email'  => 'employee@domain.com',
+                'role_id'  => 1,
+                'password'  => Hash::make('password'),
+            ],
+            [
+                'name' => 'reader',
+                'email'  => 'reader@domain.com',
+                'role_id'  => 2,
+                'password'  => Hash::make('password'),
+            ],
         ]);
     }
 }

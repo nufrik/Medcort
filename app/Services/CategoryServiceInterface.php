a6ab3e1152136af17b93dccda2f09b0711853f5e
nufrik <daniilonufriev30@gmail.com>
2023-04-28 12:27:01 +0300

    added 3 actions

--- app/Services/CategoryServiceInterface.php
+++ app/Services/CategoryServiceInterface.php
@@ -3,9 +3,17 @@
 namespace App\Services;
 
 
+use App\Http\Requests\Category\CreateRequest;
+use App\Http\Requests\Category\EditRequest;
 use Illuminate\Database\Eloquent\Collection;
 
 interface CategoryServiceInterface
 {
     public function getByAll() : Collection;
+
+    public function createCategory(CreateRequest $request) : bool;
+
+    public function editCategoryById(int $id, EditRequest $request) : bool;
+
+    public function deleteCategoryById(int $id) : bool;
 }

3854ab3e3a4f81a86e09d692f6d05cb0e4f1b98f
nufrik <daniilonufriev30@gmail.com>
2023-04-28 22:44:30 +0300

    added 3 new actions createBook,editBookById,deleteBookById

--- app/Services/BookServiceInterface.php
+++ app/Services/BookServiceInterface.php
@@ -2,6 +2,8 @@
 
 namespace App\Services;
 
+use App\Http\Requests\Book\CreateRequest;
+use App\Http\Requests\Book\EditRequest;
 use App\Models\Books;
 use Illuminate\Pagination\LengthAwarePaginator;
 
@@ -9,7 +11,11 @@ interface BookServiceInterface
 {
     public function getBooksByCategorySlug(string $slug) : LengthAwarePaginator;
 
-    /*public function getBookByCategorySlug(string $slug, string $bookSlug) : Books;*/
-
     public function getBookBySlug(string $slug) : Books;
+
+    public function createBook(CreateRequest $request) : bool;
+
+    public function editBookById(int $id, EditRequest $request) : bool;
+
+    public function deleteBookById(int $id) : bool;
 }

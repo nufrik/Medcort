c32c200480dc51815c390f84f24c36df499c24e6
nufrik <daniilonufriev30@gmail.com>
2023-04-28 22:49:15 +0300

    new file validator

---
+++ app/Http/Requests/Book/EditRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests\Book;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class EditRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => ['required', 'string', 'max:30'],
+            'author' => ['required', 'string', 'max:50'],
+            'description' => ['required', 'string', 'max:10000'],
+            'rating' => [ 'required', 'numeric'],
+            'cover' => ['required', 'image'],
+        ];
+    }
+}

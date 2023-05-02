c86379143ae89afa85896a84d7dde5e5c168b92a
nufrik <daniilonufriev30@gmail.com>
2023-05-02 18:48:26 +0300

    added a new relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,4 +46,9 @@ class User extends Authenticatable
     {
         return $this->belongsTo(Roles::class);
     }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }

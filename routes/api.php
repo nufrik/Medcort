a59cea330f3acd1afe8491fc91f90c3f9f59f20d
nufrik <daniilonufriev30@gmail.com>
2023-05-02 18:42:45 +0300

    added new route

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\V1\BookController;
+use App\Http\Controllers\Api\V1\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResources([
+    'books' => BookController::class,
+    'categories' => CategoryController::class,
+]);

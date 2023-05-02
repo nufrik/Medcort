3d20a4bead83b1e31f4eb4477f1a49d66bb3b840
nufrik <daniilonufriev30@gmail.com>
2023-05-02 12:43:40 +0300

    added 2 actions

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -2,12 +2,34 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Services\EmployeeService;
+
 
 class EmployeeController extends Controller
 {
+    private EmployeeService $employeeService;
+
+    public function __construct(EmployeeService $employeeService)
+    {
+        $this->employeeService = $employeeService;
+    }
+
     public function employee()
     {
         return view('employee');
+    }
+
+    public function showCategories()
+    {
+        return view('employee-categories',[
+            'categories' => $this->employeeService->getAllCategories(),
+        ]);
+    }
+
+    public function showBooks()
+    {
+        return view('employee-books',[
+            'books' => $this->employeeService->getAllBooks(),
+        ]);
     }
 }
